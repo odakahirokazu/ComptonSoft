071c30c345621985b022f27938e7d24293534f96
Hirokazu Odaka <hirokazu1odaka@gmail.com>
2026-04-01 16:56:02 +0900

    Fix PolarizedXrayEvent::calculateEventAngle()

--- source/core/src/PolarizedXrayEvent.cc
+++ source/core/src/PolarizedXrayEvent.cc
@@ -45,16 +45,12 @@ double PolarizedXrayEvent::calculateEventAngle() const
   using anlgeant4::constant::halfpi;
 
   const int size = EventSize();
-  const int center = size/2;
-
   double xsum(0.0), ysum(0.0), esum(0.0);
   for (int i=0; i<size; i++) {
     for (int j=0; j<size; j++) {
       const double epi = Data()[i][j];
-      const double x1 = static_cast<double>(i-center);
-      const double y1 = static_cast<double>(j-center);
-      xsum += x1*epi;
-      ysum += y1*epi;
+      xsum += i*epi;
+      ysum += j*epi;
       esum += epi;
     }
   }
